e9f426b67931a499235bd50bb2e76f45e1e9ba03
DESKTOP-TEM8N3F\Mutti8888 <taavi.makela@gmail.com>
2023-02-17 18:17:49 +0200

    nappi tehty

--- 2023kevatpe1715_react-app/src/pages/Message.jsx
+++ 2023kevatpe1715_react-app/src/pages/Message.jsx
@@ -3,8 +3,7 @@ import React    from "react";
 export const Message = () =>{
     return(
         <div>
-            <h1>Hello  World!</h1>
-            
+            <h1>Hello  World!</h1>        
         </div>
     );
 }
