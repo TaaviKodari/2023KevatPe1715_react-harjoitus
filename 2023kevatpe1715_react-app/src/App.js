d1291e5e81261502a611c4a549c2664c3b99527b
DESKTOP-TEM8N3F\Mutti8888 <taavi.makela@gmail.com>
2023-02-24 18:25:49 +0200

    router tehty

--- 2023kevatpe1715_react-app/src/App.js
+++ 2023kevatpe1715_react-app/src/App.js
@@ -1,13 +1,21 @@
 import React from "react";
 import {Message} from  "./pages/Message";
 import {Buttons} from "./pages/Buttons";
+import {Router} from "./pages/Router";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div >
-     <Message/>
-     <Buttons/>
+  <BrowserRouter>
+    <div>
+      <Routes>
+        <Route path='/' element={<Router/>}>
+          <Route index element={<Message/>}/>
+          <Route path ='Buttons' element={<Buttons/>}/>
+        </Route> 
+      </Routes>
     </div>
+    </BrowserRouter>
   );
 }
 
