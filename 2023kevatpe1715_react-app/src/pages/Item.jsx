77fa1751ce5d8341773020315af7fd374ccdb8ca
DESKTOP-TEM8N3F\Mutti8888 <taavi.makela@gmail.com>
2023-03-17 18:17:12 +0200

    item aloitettu

---
+++ 2023kevatpe1715_react-app/src/pages/Item.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Item = (props) =>{
+    return(
+        <div>
+
+        </div>
+    );
+}
---
+++ 2023kevatpe1715_react-app/src/pages/ItemDatabase.js
@@ -0,0 +1,12 @@
+export const items =[
+    {
+        img:"https://cdn.pixabay.com/photo/2022/10/26/03/30/fog-7547298__340.jpg"
+        ,
+        id:0,
+    },
+    {
+        img:"https://cdn.pixabay.com/photo/2022/10/18/17/56/bird-7530845_960_720.jpg"
+        ,
+        id:1,
+    }
+]
