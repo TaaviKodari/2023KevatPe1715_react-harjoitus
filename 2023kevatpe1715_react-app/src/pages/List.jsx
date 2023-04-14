061071eb42975626a7a53cf4c1bb7273de6f4609
DESKTOP-TEM8N3F\Mutti8888 <taavi.makela@gmail.com>
2023-04-14 18:16:56 +0300

    List valmis

--- 2023kevatpe1715_react-app/src/pages/List.jsx
+++ 2023kevatpe1715_react-app/src/pages/List.jsx
@@ -4,6 +4,18 @@ import { useState } from "react";
 export const List = () =>{
     const [content, setContent] = useState([]);
     const [inputValue, setInputValue] = useState('');
+
+    function clearList(){
+        setContent([]);
+    }
+
+    const removeHandler = (removeIndex)=>{
+        const removeItem = content.filter((item, index)=>{
+                return removeIndex !== index;
+        });
+        setContent(removeItem);
+    }
+
     return(
         <div>
             <div>
@@ -16,9 +28,24 @@ export const List = () =>{
                     <input type = "text"
                      placeholder="Type here..."
                       value={inputValue}
+                      onChange={e=> setInputValue(e.target.value)}
                       required/>
                     <input type = "submit" value="Submit"/>
+                    {
+                        content.map((item,index)=>(
+                            <ul>
+                                <li>
+                                    <h2 key={index}>{item}</h2>
+                                    <button
+                                    form="eri formi" 
+                                    onClick={()=>removeHandler(index)}
+                                    >Remove</button>
+                                </li>
+                            </ul>
+                        ))
+                    }
                 </form>
+                <button onClick={clearList}>Clear List</button>
             </div>
         </div>
     );
