146765673e4a73068c685a433840d9cb10579ace
DESKTOP-TEM8N3F\Mutti8888 <taavi.makela@gmail.com>
2023-05-05 18:22:58 +0300

    onSquareClick tehty

--- 2023kevatpe1715_react-app/src/TicTacToe/TicTacToe.jsx
+++ 2023kevatpe1715_react-app/src/TicTacToe/TicTacToe.jsx
@@ -2,12 +2,29 @@ import React from "react";
 import './TicTacToe.css';
 import { Board } from "./Board";
 import { useState } from "react";
+const initialBoard = ['','','','','','','','',''];
+
 export const TicTacToe =()=>{
+    const[gameState, setGameState] = useState(initialBoard);
+    const[isXTurn, setIsXTurn] = useState(true);
+
+    const onSquareClick = (index) =>{
+        let strings = Array.from(gameState);
+        if(strings[index] !== ''){
+            return;
+        }
+        
+        strings[index] = isXTurn ? 'X' : 'O';
+        setGameState(strings);
+        setIsXTurn(!isXTurn);
+
+    }
+
     return(
         <div>
             <div className="game">
                 <h1>TIC-TAC-TOE</h1>
-                <Board/>
+                <Board gameState={gameState} onSquareClick={onSquareClick}/>
             </div>
         </div>
     );
