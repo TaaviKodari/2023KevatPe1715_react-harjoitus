52794f5fcf8e8b41719b3afe649d43281ba6b7dc
DESKTOP-TEM8N3F\Mutti8888 <taavi.makela@gmail.com>
2023-05-12 18:15:41 +0300

    useEffect kesken

--- 2023kevatpe1715_react-app/src/TicTacToe/Board.jsx
+++ 2023kevatpe1715_react-app/src/TicTacToe/Board.jsx
@@ -11,30 +11,37 @@ export const Board =({gameState, onSquareClick})=>{
                 />
                 <Square className="b-bottom-r"
                 state={gameState[1]}
+                onClick={()=> onSquareClick(1)}
                 />
                 <Square className="b-bottom"
                 state={gameState[2]}
+                onClick={()=> onSquareClick(2)}
                 />
             </div>
             <div className="row">
                 <Square className="b-bottom-r"
                 state={gameState[3]}
+                onClick={()=> onSquareClick(3)}
                 />
                 <Square className="b-bottom-r"
                 state={gameState[4]}
+                onClick={()=> onSquareClick(4)}
                 />
                 <Square className="b-bottom"
                 state={gameState[5]}
+                onClick={()=> onSquareClick(5)}
                 />
             </div>
             <div className="row">
                 <Square className="b-right"
                 state={gameState[6]}
+                onClick={()=> onSquareClick(6)}
                 />
                 <Square className="b-right"
                 state={gameState[7]}
+                onClick={()=> onSquareClick(7)}
                 />
-                <Square state={gameState[8]}/>
+                <Square state={gameState[8]} onClick={()=> onSquareClick(8)}/>
             </div>
         </div>
     );
